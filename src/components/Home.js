714ef578482c9548aa179e6f2f740ece43cb7cb4
Brian Dokas <dokasbri@msu.edu>
2020-09-03 22:13:26 -0400

    Adding icons for linkedin and github

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
- 
+import pic from './media/DSC_0831 (2).jpg'
+
 const home = () => {
     return (
-       <div>
-          <h1>Home</h1>
-           <p>Home page body content</p>
-       </div>
+        <div>
+            <h1>Home</h1>
+            <p>Home page body content</p>
+            <img src={pic} alt='Test'/>
+        </div>
     );
 }
- 
+
 export default home;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,33 @@
-import React from 'react';
- 
-import { NavLink } from 'react-router-dom';
- 
+import React from "react";
+import LinkedinIcon from "react-ionicons/lib/LogoLinkedin";
+import GithubIcon from "react-ionicons/lib/LogoGithub";
+
+import { NavLink } from "react-router-dom";
+
 const Navigation = () => {
-    return (
-       <div>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
-       </div>
-    );
-}
- 
+  return (
+    <div>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+      <LinkedinIcon
+        name="logo-linkedin"
+        onClick={() =>
+          window.location.assign(
+            "https://www.linkedin.com/in/brian-dokas-ba8aaa190/"
+          )
+        }
+      ></LinkedinIcon>
+      <GithubIcon
+         name="logo-github"
+         onClick={() =>
+            window.location.assign(
+               "https://github.com/BDokas"
+            )
+         }
+      ></GithubIcon>
+    </div>
+  );
+};
+
 export default Navigation;
