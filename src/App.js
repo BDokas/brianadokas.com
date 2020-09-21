d012c0de749c3a116d558962e77d8b6db7f4fceb
Brian Dokas <dokasbri@msu.edu>
2020-09-21 00:50:37 -0400

    Added photography portfolio

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
+import Portfolio from './Components/Portfolio';
 import Contact from './Components/Contact';
 
 class App extends Component {
@@ -13,7 +14,6 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      foo: 'bar',
       resumeData: {}
     };
 
@@ -47,6 +47,7 @@ class App extends Component {
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
