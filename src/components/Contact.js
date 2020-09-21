2bb6f64319ad9643dc0e74286d26b7ca34312cfd
Brian Dokas <dokasbri@msu.edu>
2020-09-21 15:40:17 -0400

    email send functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,95 +1,149 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import emailjs from "emailjs-com";
 
 class Contact extends Component {
-  render() {
+   render() {
+      if (this.props.data) {
+         var name = this.props.data.name;
+         var city = this.props.data.address.city;
+         var state = this.props.data.address.state;
+         var zip = this.props.data.address.zip;
+         var phone = this.props.data.phone;
+         var email = this.props.data.email;
+         var message = this.props.data.contactmessage;
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var message = this.props.data.contactmessage;
-    }
+         emailjs.init("user_GfzZLyLoK8unCI3uumLrv");
+      }
 
-    return (
-      <section id="contact">
+      function sendEmail(e) {
+         e.preventDefault();
+         var send_button = document.getElementById("submit_button");
+         emailjs
+            .sendForm(
+               "contact_brian",
+               "contact_form",
+               e.target,
+               "user_GfzZLyLoK8unCI3uumLrv"
+            )
+            .then(
+               (result) => {
+                  send_button.style.color = "green";
+                  send_button.value = "Sent!";
+                  console.log(result.text);
+               },
+               (error) => {
+                  send_button.style.color = "red";
+                  send_button.value = "Send Failed";
+                  console.log(error.text);
+               }
+            );
+      }
 
-         <div className="row section-head">
+      return (
+         <section id="contact">
+            <div className="row section-head">
+               <div className="two columns header-col">
+                  <h1>
+                     <span>Get In Touch.</span>
+                  </h1>
+               </div>
 
-            <div className="two columns header-col">
-
-               <h1><span>Get In Touch.</span></h1>
-
-            </div>
-
-            <div className="ten columns">
-
+               <div className="ten columns">
                   <p className="lead">{message}</p>
-
+               </div>
             </div>
 
-         </div>
+            <div className="row">
+               <div className="eight columns">
+                  <form
+                     className="contact-form"
+                     id="contactForm"
+                     name="contactForm"
+                     onSubmit={sendEmail}
+                  >
+                     <fieldset>
+                        <div>
+                           <label htmlFor="contactName">
+                              Name <span className="required">*</span>
+                           </label>
+                           <input
+                              type="text"
+                              defaultValue=""
+                              size="35"
+                              id="contactName"
+                              name="from_name"
+                              required
+                           />
+                        </div>
 
-         <div className="row">
-            <div className="eight columns">
+                        <div>
+                           <label htmlFor="contactEmail">
+                              Email <span className="required">*</span>
+                           </label>
+                           <input
+                              type="text"
+                              defaultValue=""
+                              size="35"
+                              id="contactEmail"
+                              name="user_email"
+                              required
+                           />
+                        </div>
 
-               <form action="" method="post" id="contactForm" name="contactForm">
-					<fieldset>
+                        <div>
+                           <label htmlFor="contactSubject">Subject</label>
+                           <input
+                              type="text"
+                              defaultValue=""
+                              size="35"
+                              id="contactSubject"
+                              name="contactSubject"
+                           />
+                        </div>
 
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
+                        <div>
+                           <label htmlFor="contactMessage">
+                              Message <span className="required">*</span>
+                           </label>
+                           <textarea
+                              cols="50"
+                              rows="15"
+                              id="contactMessage"
+                              name="message"
+                              required
+                           ></textarea>
+                        </div>
+
+                        <div>
+                           <input id="submit_button" className="submit" type="submit" value="Send"/>
+                        </div>
+
+                     </fieldset>
+                  </form>
+
+                  {/* <div id="message-warning"> Error boy</div> */}
+                  {/* <div id="message-success">
+                     <i className="fa fa-check"></i>Your message was sent, thank you!
+              <br />
+                  </div> */}
+               </div>
+
+               <aside className="four columns footer-widgets">
+                  <div className="widget widget_contact">
+                     <h4>Email and Phone</h4>
+                     <p className="address">
+                        {email}
+                        <br />
+                        {city}, {state} {zip}
+                        <br />
+                        <span>{phone}</span>
+                     </p>
                   </div>
-
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
-
-                  <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
-				   </form>
-
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
-
-
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
-
-					   <h4>Email and Phone</h4>
-					   <p className="address">
-						   {email}<br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
-					   </p>
-				   </div>
-            </aside>
-      </div>
-   </section>
-    );
-  }
+               </aside>
+            </div>
+         </section>
+      );
+   }
 }
 
 export default Contact;
